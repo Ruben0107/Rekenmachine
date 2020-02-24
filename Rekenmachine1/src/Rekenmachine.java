499502c7ee6ce70b90a5ba77a5bf550c23106424
Shinjice <tdna84@gmail.com>
2020-02-24 14:20:36 +0100

    startbestand main

--- Rekenmachine1/src/Rekenmachine.java
+++ Rekenmachine1/src/Rekenmachine.java
@@ -2,8 +2,16 @@
 public class Rekenmachine {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Michiel a = new Michiel;
+		Ruben b = new Ruben;
+		
+		
+		
+		b.optellen(5, 6);
+		b.aftrekken(5, 7);
+		a.delen(6, 7);
+		a.vermenigvuldigen(5, 6);
 	}
 
 }
