559b846e5066ff4064452c2b0259aae78fd908d3
Ruben0107 <rubenvanrij@gmail.com>
2020-02-24 15:00:46 +0100

    go3

---
+++ Rekenmachine1/src/Ruben.java
@@ -0,0 +1,17 @@
+
+public class Ruben {
+	
+	public void optellen(int a, int b) {
+		
+		System.out.println("de optelsom van" + a + " + " + b + "is" + (a + b));
+		
+	}
+	
+	public void aftrekken(int a, int b) {
+		
+		System.out.println("de optelsom van" + a + " - " + b + "is" + (a - b));
+		
+	}
+	
+		
+}
